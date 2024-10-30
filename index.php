283ae72b9f40621009200d51d8e5b338b124e236
filipeagneloo <filipe.agnelo@uni9.edu.br>
2024-10-30 19:00:49 -0300

    index.php

--- index.php
+++ index.php
@@ -9,41 +9,71 @@
     <h1>Calculadora AJAX</h1>
 
     <pre>
-    <label>Numero 01</label>
-    <input type="text" name="numero1" id="numero1"/>
+    <fieldset>
+        <legend>Área cômodo</legend>
 
-    <label>Numero 02</label>
-    <input type="text" name="numero2" id="numero2"/>
+    <label>Largura (m²)</label>
+    <input type="number" name="larguraComodo" id="larguraComodo"/>
 
-    <button>Calcular com ajax</button>
+    <label>Comprimento (m²)</label>
+    <input type="number" name="comprimentoComodo" id="comprimentoComodo"/>
+    </fieldset>
+
+    <fieldset>
+        <legend>Tamanho Piso/Azuleijo</legend>
+
+    <label>Largura (m²)</label>
+    <input type="number" name="larguraPiso" id="larguraPiso"/>
+
+    <label>Comprimento (m²)</label>
+    <input type="number" name="comprimentoPiso" id="comprimentoPiso"/>
+    </fieldset>
+
+    <label>Margem de erro (%)</label>
+    <input type="number" name="margem" id="margem"/>
+
+    <button onclick="calcular();">Calcular com ajax</button>
 
     <p id="resultado"></p>
     </pre>
     <script>
         function calcular(){
-            const numero1 = document.getElementById("numero1").value;
-            const numero2 = document.getElementById("numero2").value;
+            const larguraComodo = document.getElementById("larguraComodo").value;
+            const comprimentoComodo = document.getElementById("comprimentoComodo").value;
+            const larguraPiso = document.getElementById("larguraPiso").value;
+            const comprimentoPiso = document.getElementById("comprimentoPiso").value;
+            const margem = document.getElementById("margem").value;
 
+            const medidas = {
+                larguraComodo,
+                comprimentoComodo,
+                larguraPiso,
+                comprimentoPiso,
+                margem
+            };
+
+            const json = JSON.stringify(medidas);
+                       
             fetch('/calculo.php', {
                 method: 'POST',
-                headers: ( 'Content-Type': 'application/json'),
-                body: JSON.stringfy({
-                    numero1: parseFloat(numero1),
-                    numero2: parseFloat(numero2)
-                })
+                headers: { 'Content-Type': 'application/json'},
+                body: json
             })
             .then(resposta => resposta.json())
-            .then(dados=>{
-
-                document.getElementById("resultado").innerHTML =
-                    "Soma: " + dados.soma;
+            .then(dados =>{
+               
+                document.getElementById("resultado").innerHTML = 
+                   "areaComodo: " + dados.areaComodo +
+                      "areaPiso: " + dados.areaPiso +
+                      "quantidade: " + dados.quantidade +
+                      "margem: " + dados.margem +
+                      "total: " + dados.total;
             })
-            .catch(erro=>{
-                document.getElementById("resultado").innerHTML =
-                "Erro ao processar"
+            .catch(erro =>{
+                document.getElementById("resultado").innerHTML = 
+                "Erro ao processar";
             });
         }
-
     </script>
 </body>
 </html>
